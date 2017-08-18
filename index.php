3c5641e8094c75beb01ebd5442edcbdd3d469cff
-Mayonis masin- <solehinjr@gmail.com>
2017-08-18 17:19:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  <!DOCTYPE html>  
  <html>  
       <head>  
-           <title>Webslesson Tutorial | PHP AJAX Jquery - Load Dynamic Content in Bootstrap Popover</title>  
+           <title> PHP AJAX Jquery - Load Dynamic Content in Bootstrap Popover</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
